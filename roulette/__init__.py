de9ace2d4c295994664c31b4a8927dcb009fac11
Lee-matod <lee.underscore.matod@gmail.com>
2026-02-24 17:48:51 -0300

    Add project metadata

--- roulette/__init__.py
+++ roulette/__init__.py
@@ -4,3 +4,9 @@ from roulette.error import *
 from roulette.roulette import *
 
 __all__ = ("Bet", "InvalidBet", "NotEnoughFunds", "Roulette", "RouletteException", "SplitBetType")
+
+__title__ = "roulette"
+__author__ = "Lee-matod"
+__license__ = "MIT"
+__copyright__ = "Copyright 2026-present Lee (Lee-matod)"
+__version__ = "1.0.0"
